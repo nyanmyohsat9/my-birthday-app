a3acccf508f8a83bf36803d22f4834822c46890e
nyanmyohsat9 <nyanmyohsat@outlook.com>
2025-06-01 20:50:52 +0630

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
 import CinematicBirthdayCard from './components/CinematicBirthdayCard'
 
-function App() {
+export default function App() {
   return <CinematicBirthdayCard />
 }
-
-export default App
